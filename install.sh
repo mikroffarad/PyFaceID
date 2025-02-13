c30ba9664776fbfac60e81958cbb8da92981196f
Victor Zaritsky <42099665+mikroffarad@users.noreply.github.com>
2025-02-14 00:21:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y python3 python3-dev python3-venv python3-tk cmake build-essential
+sudo apt install -y python3 python3-dev python3-venv cmake build-essential qt6-base-dev libxcb-cursor-dev
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
@@ -14,7 +14,7 @@ source $SCRIPT_DIR/venv/bin/activate
 
 echo "$SCRIPT DIR"
 
-pip install opencv-python face_recognition 
+pip install opencv-python face_recognition pyside6
 
 pip install wheel setuptools pip --upgrade 
 pip install git+https://github.com/ageitgey/face_recognition_models --verbose
